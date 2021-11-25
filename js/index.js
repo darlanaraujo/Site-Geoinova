ba10a6df8623364f62f78eb502f26a77162e0c81
Darlan Araujo <darlanparaujo@outlook.com>
2021-11-25 17:52:55 -0300

    Atualização final topo

--- js/index.js
+++ js/index.js
@@ -22,3 +22,46 @@ ativaScrollSuave('a[href*=boxSolucoes]');
 
 //=========================================================================================================
 
+//Comando para pegar a posição da class containerConteudo. Essa posição será usada para manipular o topo do site;
+
+$(document).ready(function(){
+    var scroll_start = 0;
+    var containerConteudo = $("#containerConteudo");
+    var offset = containerConteudo.offset();
+
+    $(document).scroll(function(){
+        scroll_start = $(this).scrollTop();
+
+        if(scroll_start > offset.top) {
+            $("#nav").css("backgroundColor", "#fff");
+            $(".logo").css("display", "none");
+            $(".logo2").css("display", "flex");
+            $(".logo2").css("float", "left");
+            $(".btnMenuBar i").css("color", "#3791ff");
+            $(".menuTopo a").css("color", "#443d3d");
+        } else {
+            $("#nav").css("backgroundColor", "transparent");
+            $(".logo2").css("display", "none");
+            $(".logo").css("display", "block");
+            $(".btnMenuBar i").css("color", "#fff");
+            $(".menuTopo a").css("color", "#fff");
+        }
+    });
+});
+
+/*
+const box = document.querySelector("#boxSolucoes")
+
+var topo = document.querySelectorAll("#topo");
+
+const posicao = box.getBoundingClientRect();
+
+console.log(posicao);
+
+if(posicao.top != 601.2000122070312){
+    console.log("A pagina mexeu!");
+    document.nav.style.backgroudColor = "red";
+} else{
+    console.log("A pagina NÃO mexeu!");
+}
+*/
